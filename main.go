1540762c8bdbd8eaf4050986c1ce4db4a2112561
Samantha Baldwin <samantha_baldwin@mckinsey.com>
2020-08-06 12:00:02 -0400

    fix(main): remove duplicate import

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"github.com/hashicorp/terraform/plugin"
 	"github.com/hashicorp/terraform-plugin-sdk/plugin"
 	"github.com/samanthaq/terraform-provider-circleci/circleci"
 )
